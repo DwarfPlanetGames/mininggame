a896a3869838091e861f0cf5299d79eedb92def3
Brandon Dyer <brandondyer64@gmail.com>
2015-12-17 11:15:02 -0500

    Signed-off-by: Brandon Dyer <brandondyer64@gmail.com>

---
+++ android/src/ml/dpgames/mine/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ml.dpgames.mine.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ml.dpgames.mine.MGMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MGMain(), config);
+	}
+}
