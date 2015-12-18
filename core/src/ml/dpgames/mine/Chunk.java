0d7e1a6344b87cfaff108551aa8164e081cf7866
Brandon Dyer <brandondyer64@gmail.com>
2015-12-18 15:38:23 -0500

    Signed-off-by: Brandon Dyer <brandondyer64@gmail.com>

--- core/src/ml/dpgames/mine/Chunk.java
+++ core/src/ml/dpgames/mine/Chunk.java
@@ -7,7 +7,8 @@ import ml.dpgames.mine.objects.Block;
 import com.badlogic.gdx.math.Vector2;
 
 public class Chunk {
-
+	
+	public static final int chunkSize = 8;
 	private final HashMap<Vector2, Block> blocks = new HashMap<Vector2, Block>();
 
 	public Block getBlock(int x, int y) {
@@ -18,6 +19,9 @@ public class Chunk {
 	}
 
 	public Block replace(int x, int y, Block block) {
+		if (!(x >= 0 && y >= 0 && x < chunkSize && y < chunkSize)) {
+			throw new ArrayIndexOutOfBoundsException(x > y ? ("x is " + x) : ("y is " + y));
+		}
 		Block ret = null;
 		if (blocks.containsKey(new Vector2(x,y))) {
 			ret = blocks.get(new Vector2(x,y));
